73c49b0df8efa3812a3466526dcb4e0dbb841263
trang <101734306+trangha2710@users.noreply.github.com>
2025-12-17 23:29:08 -0500

    fix: fix message

--- element.sh
+++ element.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 if [[ -z $1 ]]; then
-  echo "Please provide an element atomic number, symbol, or name as an argument."
+  echo "Please provide an element as an argument."
   exit 0
 fi
 INPUT_VALUE="$1"
