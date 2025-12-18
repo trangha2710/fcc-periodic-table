75d8b74c869fa8b9c61afb0907daa3d856dfdbe4
trang <101734306+trangha2710@users.noreply.github.com>
2025-12-17 23:11:33 -0500

    fix: db setting

--- element.sh
+++ element.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-echo "Please provide an element as an argument."
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+if [[ -z $1 ]]; then
+  echo "Please provide an element atomic number, symbol, or name as an argument."
+  exit 0
+fi
